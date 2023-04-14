cd50245619d5ae14913b3aada5ac02ba1accb0e4
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-15 02:04:48 +0530

    refactored homepage
    refactored homePage.html
    refactored homePage.css
    added homePage.js

---
+++ homePage.js
@@ -0,0 +1,17 @@
+let submitBtn = document.querySelector(".submitBtn");
+let voterId = document.querySelector(".voterId");
+let text = document.querySelector(".content h3");
+
+let regex = /^[A-Z]{3}[0-9]{7}$/;
+
+submitBtn.addEventListener("click", ()=>{
+   if(voterId.value == ""){
+    text.innerText = "Please Enter a Voter Id";
+   }
+    else if(voterId.value.match(regex)){
+        text.innerText = "Valid Voter Id"
+    }
+    else{
+        text.innerText = "Invalid Voter Id";
+    }
+}); 
