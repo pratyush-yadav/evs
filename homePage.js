aeddb8b7c02291a9ab43613bc4ad047c751941cb
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-15 16:41:32 +0530

    update home page
    converted spaces to tabs

--- homePage.js
+++ homePage.js
@@ -4,14 +4,14 @@ let text = document.querySelector(".content h3");
 
 let regex = /^[A-Z]{3}[0-9]{7}$/;
 
-submitBtn.addEventListener("click", ()=>{
-   if(voterId.value == ""){
-    text.innerText = "Please Enter a Voter Id";
-   }
-    else if(voterId.value.match(regex)){
-        text.innerText = "Valid Voter Id"
-    }
-    else{
-        text.innerText = "Invalid Voter Id";
-    }
+submitBtn.addEventListener("click", () => {
+	if (voterId.value == "") {
+		text.innerText = "Please Enter a Voter Id";
+	}
+	else if (voterId.value.match(regex)) {
+		text.innerText = "Valid Voter Id"
+	}
+	else {
+		text.innerText = "Invalid Voter Id";
+	}
 }); 
