072819cd417ad7ab167d1441142551f6671e1551
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-21 17:38:35 +0530

    updated urls.py and views.py

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def votingPage(request):
+    return render(request, "votingPage.html")
