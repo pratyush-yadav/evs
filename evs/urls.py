f0b0fb824a8bb99ee1bcc68690bba5679f00fc35
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-21 17:35:06 +0530

    create index.html
    updated both urls.py to account for index.html
    updated base.py to point to correct URLs
    updated views.py to render index.html

--- evs/urls.py
+++ evs/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('home', include('home.urls')),
 ]
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
 ]
