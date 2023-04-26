7df08186c36618d51ea5afe98a85db4587f94f27
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-26 13:02:03 +0530

    added a superuser

--- evs/urls.py
+++ evs/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "E Voting System"
+admin.site.site_title = "E Voting System"
+admin.site.index_title = "E Voting System"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
