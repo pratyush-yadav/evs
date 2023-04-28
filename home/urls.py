bf38cf7eef41b647aeffbaad32e612337db07a1f
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-04-29 00:48:40 +0530

    added path for login in urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('about', views.about, name='about'),
     path('vote', views.vote, name='vote'),
+    path("login", views.login, name="login"),
 ]
