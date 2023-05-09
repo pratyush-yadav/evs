7fe43a585eb806ba341630fa16a37b7616d009be
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-05-09 22:11:31 +0530

    remove send_otp and check_otp route

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('about', views.about, name='about'),
     path('vote', views.vote, name='vote'),
-    path("send_otp", views.send_otp, name="send_otp"),
-    path("check_otp", views.check_otp, name="check_otp"),
     path("waitPage", views.waitPage, name="waitPage"),
 ]
