f99aa47b0d730134784c5aa4cf40bdab3d911934
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-05-09 22:12:54 +0530

    add route for contact page

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('vote', views.vote, name='vote'),
     path("waitPage", views.waitPage, name="waitPage"),
 ]
