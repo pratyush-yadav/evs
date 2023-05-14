d2089758a6949a990e457d68be7a5ba8b0f3ec15
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-05-14 15:17:17 +0530

    remove all messages provided by django

--- home/views.py
+++ home/views.py
@@ -18,14 +18,12 @@ def index(request):
         if entered_otp == sent_otp:
             # User entered corret OTP...
             request.session["authentication_status"] = "logged_in"
-            messages.success(request, "User logged in successfully !!!")
             # redirection will be based on the voting status as per user and date/time...
             # will be working on it later...
             return redirect("vote")
         else:
             # User entered incorret OTP...
             request.session["authentication_status"] = "login_failed"
-            messages.error(request, "Incorrect OTP !!!")
             return render(request, "otp.html")
         
     else:
@@ -33,7 +31,6 @@ def index(request):
         request.session["authentication_status"] = "not_logged_in"
         request.session["voter_id"] = None
         request.session["otp"] = None
-        messages.info(request, "Info: Site normally visited...")
         return render(request, "index.html")
 
 
