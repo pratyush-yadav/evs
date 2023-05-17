08e49ddcd01b8948483f0ac892c0bfed76f7846f
Pratyush Yadav <91780729+pratyush-yadav@users.noreply.github.com>
2023-05-17 20:27:08 +0530

    create voters model and register it
    also make migrations and migrate

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Voters(models.Model):
+    voter_id = models.CharField(max_length=10)
+    aadhar = models.CharField(max_length=12)
+    name = models.CharField(max_length=100)
+    dob = models.DateField()
+    gender = models.CharField(max_length=6)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.voter_id} {self.name}"
